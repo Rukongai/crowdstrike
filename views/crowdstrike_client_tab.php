1f13b384f0a904926f402555edd437e7550cc6b7
dcoobs <12276948+dcoobs@users.noreply.github.com>
2020-07-21 13:19:47 -0500

    Update for sensor transition to falconctl
    Removed installguard status

--- views/crowdstrike_client_tab.php
+++ views/crowdstrike_client_tab.php
@@ -17,10 +17,6 @@
                     <th data-i18n="crowdstrike.customer_id"></th>
                     <td id="crowdstrike-customer_id"></td>
                 </tr>
-                <tr>
-                    <th data-i18n="crowdstrike.sensor_installguard"></th>
-                    <td id="crowdstrike-sensor_installguard"></td>
-                </tr>
             </table>
         </div>
         <div class="col-md-6">
@@ -40,15 +36,6 @@ $(document).on('appReady', function(e, lang) {
             $('#crowdstrike-sensor_id').text(data.sensor_id);
             $('#crowdstrike-sensor_version').text(data.sensor_version);
             $('#crowdstrike-customer_id').text(data.customer_id);
-            $('#crowdstrike-sensor_installguard').text(data.sensor_installguard);
-
-            if(data.sensor_installguard === "0" ) {
-                $('#crowdstrike-sensor_installguard').text("Disabled");
-            } else if(data.sensor_installguard === "1" ) {
-                $('#crowdstrike-sensor_installguard').text("Enabled");
-            } else{
-                 $('#crowdstrike-sensor_installguard').text(data.sensor_installguard);
-            } 
         });
 });
 
