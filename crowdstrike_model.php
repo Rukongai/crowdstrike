8f3ac20fa45a49df3c789d991e86425189df6195
dcoobs <12276948+dcoobs@users.noreply.github.com>
2020-07-21 13:15:30 -0500

    Update for sensor transition to falconctl
    Removed installguard status

--- crowdstrike_model.php
+++ crowdstrike_model.php
@@ -12,7 +12,6 @@ class Crowdstrike_model extends \Model {
                 $this->rs['sensor_id'] = '';
                 $this->rs['sensor_version'] = '';
                 $this->rs['customer_id'] = '';
-                $this->rs['sensor_installguard'] = 0; //boolean
 
                 if ($serial) {
                     $this->retrieve_record($serial);
@@ -38,16 +37,7 @@ class Crowdstrike_model extends \Model {
              $this->sensor_id = $plist['sensor_id'];
              $this->sensor_version = $plist['sensor_version'];
              $this->customer_id = $plist['customer_id'];
-             $this->sensor_installguard = $plist['sensor_installguard'];
              $this->save();
          }
 
-         public function get_crowdstrike_installguard_stats()
-         {
-             $sql = "SELECT COUNT(CASE WHEN sensor_installguard = 1 THEN 1 END) as enabled, COUNT(CASE WHEN sensor_installguard = 0 THEN 1 END) as 'disabled'
-                 FROM crowdstrike
-                 LEFT JOIN reportdata USING(serial_number)
-                 ".get_machine_group_filter();
-             return current($this->query($sql));
-         }
 }
